74ad7f76213203d4e3950feac8fbf611a458b742
Jose <jm_rona@hotmail.com>
2021-04-16 11:17:00 +0100

    Protected test done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
@@ -32,5 +31,4 @@ class UserFactory extends Factory
             'darkMode' => false,
         ];
     }
-
 }
